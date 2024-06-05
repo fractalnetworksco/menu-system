7f3d0f119874ccb9aa41e7b19b8594c94fd218ff
Jonathan <jonathan.leal1231@gmail.com>
2024-06-05 18:02:35 -0400

    changed models and had to refactor components to better fit the data from django. nearly done with final page

--- backend/menuSystem/menu/models.py
+++ backend/menuSystem/menu/models.py
@@ -18,15 +18,16 @@ class MenuItem(models.Model):
         return self.name
 
 class Note(models.Model):
-    section = models.CharField(max_length=100)
+    section = models.ForeignKey(MenuSection, on_delete=models.CASCADE, related_name='menu_notes', null=True)
     description = models.TextField()
 
     def __str__(self):
-        return self.section
+        return self.description
 
 class MenuHeader(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+
 
     def __str__(self):
         return self.title
--- backend/menuSystem/menu/serializers.py
+++ backend/menuSystem/menu/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
-from .models import MenuItem, MenuSection
+from .models import MenuItem, MenuSection, Note
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = "__all__"
 
+class MenuNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = "__all__"
+
 class MenuSectionSerializer(serializers.ModelSerializer):
     items = MenuItemSerializer(many=True)
+    menu_notes = MenuNoteSerializer(many=True)
     class Meta:
         model = MenuSection
         fields = "__all__"
