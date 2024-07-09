86359889756fe44dde6126ce5f7834be08448764
Jonathan <jonathan@fractalnetworks.co>
2024-07-09 15:58:23 -0400

    django admin changes to make items editable and changed color scheme to match little dooey colors

--- backend/menuSystem/menu/admin.py
+++ backend/menuSystem/menu/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from .models import MenuSection, MenuItem, Note, MenuHeader
 
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = [ "name", "price", "description" ]
+    list_display = ("name", "price", "description", "section")
+    list_editable = ("name", "price", "description", "section")
+    list_display_links = None
+    search_fields = ("name", "description", "section__name")
+    ordering = ("name",)
 
-admin.site.register(MenuSection)
+class MenuSectionAdmin(admin.ModelAdmin):
+    list_display = ("name", "note", "type")
+    list_editable = ("name", "note")
+    list_display_links = None
+    search_fields = ("name",)
+    ordering = ("name",)
+
+admin.site.register(MenuSection, MenuSectionAdmin)
 admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(Note)
-admin.site.register(MenuHeader)
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
--- backend/menuSystem/menuSystem/urls.py
+++ backend/menuSystem/menuSystem/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+admin.site.site_header = "The Little Dooey"
+admin.site.site_title = "Menu Board"
+admin.site.index_title = "Menu Board Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
